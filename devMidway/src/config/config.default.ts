55e2bfd94e02cd54ca9ac2b04b35859c61d9f76c
oneshark <wfrancis0525@gmail.com>
2022-06-30 15:07:27 +0800

    dev

--- devMidway/src/config/config.default.ts
+++ devMidway/src/config/config.default.ts
@@ -2,5 +2,5 @@ import { MidwayConfig } from '@midwayjs/core';
 
 export default {
   // if use http/API Gateway, please set keys here.
-  // keys: '1655127598263_2724',
+  // keys: '1656572563972_4616',
 } as MidwayConfig;
